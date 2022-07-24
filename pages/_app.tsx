1a2e0889d49a9ab946d7d86da26386dd826019d8
joaco <jgaona@keiron.cl>
2022-07-24 12:39:34 -0500

    prueba

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,15 @@ import Layout from "./components/Layout";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  console.log(process.env.GOOGLE_ANALYTICS_ID, "aki");
   return (
     <>
       <Script
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
-        id="google-analytics"
       />
 
-      <Script strategy="lazyOnload" id="google-analytics-2">
+      <Script strategy="lazyOnload" id="google-analytics">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
